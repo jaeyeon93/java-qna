596d4d6a0c44872c3679d850c4039c8cfb670a6c
JaeyeonKIM <foodsksms@gmail.com>
2018-05-22 19:29:44 +0900

    아이맥에서 푸시 파일 바뀌기 전

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,41 @@
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
-};
+// String.prototype.format = function() {
+//   var args = arguments;
+//   return this.replace(/{(\d+)}/g, function(match, number) {
+//     return typeof args[number] != 'undefined'
+//         ? args[number]
+//         : match
+//         ;
+//   });
+// };
+
+
+$(".answer-write input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+    // 서버로 바로 데이터가 들어가는것을 막는다.
+    e.preventDefault();
+    console.log("click me");
+
+    var queryString = $(".answer-write").serialize();
+    console.log("query : " + queryString);
+
+    var url = $(".answer-write").attr("action");
+    console.log("url : " + url);
+    // action에 있는 url을 가져온다.
+
+    $.ajax({
+        type : 'post',
+        url : url,
+        data : queryString,
+        dataType : 'json',
+        error : onError,
+        success : onSuccess()});
+}
+
+function onError() {
+    
+}
+
+function onSuccess(data, status) {
+    console.log(data);
+}
